b188515ed36dc1370eee544032d0e61a797c4fd3
venus00 <farhat.oussama00@gmail.com>
2025-06-24 22:43:20 +0100

    fix

--- live_client/src/App.tsx
+++ live_client/src/App.tsx
@@ -5,65 +5,63 @@ import * as mediasoupClient from 'mediasoup-client';
 const VideoViewer = () => {
   const videoRef = useRef(null);
 
- useEffect(() => {
-  const socket = io(`http://${window.location.hostname}:3009`);
-  let device;
-  let transport;
+  useEffect(() => {
+    const socket = io(`http://${window.location.hostname}:3009`);
+    let device;
+    let transport;
 
-  async function start() {
-    try {
+    async function start() {
       const rtpCapabilities = await new Promise(resolve => {
         socket.emit('getRouterRtpCapabilities', resolve);
       });
-
+      console.log(rtpCapabilities)
       device = new mediasoupClient.Device();
       await device.load({ routerRtpCapabilities: rtpCapabilities });
 
       const transportInfo = await new Promise(resolve => {
         socket.emit('createWebRtcTransport', {}, resolve);
       });
-
+      console.log(transportInfo)
       transport = device.createRecvTransport(transportInfo);
+      console.log('transport', transport)
 
       transport.on('connect', ({ dtlsParameters }, callback, errback) => {
-        socket.emit('connectWebRtcTransport', { dtlsParameters }, (response) => {
-          if (response?.error) {
-            errback(response.error);
-          } else {
-            callback();
-          }
-        });
+        socket.emit('connectWebRtcTransport', { dtlsParameters });
+        callback();
       });
 
       const consumerInfo = await new Promise(resolve => {
         socket.emit('consume', resolve);
       });
-
+      console.log("conso", consumerInfo)
       const consumer = await transport.consume({
         id: consumerInfo.id,
         producerId: consumerInfo.producerId,
         kind: consumerInfo.kind,
         rtpParameters: consumerInfo.rtpParameters,
       });
-
+      console.log(consumer)
       const stream = new MediaStream();
       stream.addTrack(consumer.track);
 
-      if (videoRef.current) {
-        videoRef.current.srcObject = stream;
-        await videoRef.current.play();
+ if (videoRef.current) {
+  videoRef.current.srcObject = stream;
+  videoRef.current.play().catch(err => {
+    console.error('Autoplay failed:', err);
+  });
+}
+
+        
       }
-    } catch (err) {
-      console.error('Error in mediasoup consumer:', err);
+      
     }
-  }
 
-  start();
+    start();
 
-  return () => {
-    socket.disconnect();
-  };
-}, []);
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <div>
@@ -71,7 +69,6 @@ const VideoViewer = () => {
       <video
         ref={videoRef}
         autoPlay
-        muted
         controls
         playsInline
         style={{ width: '100%', maxWidth: '800px' }}
